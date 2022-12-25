af81ced40141ee632c591c8030a6e3fdfe60969e
Vinhvp <71366193+Vinhvp@users.noreply.github.com>
2022-12-25 20:14:28 +0700

    Add files via upload
    xmas

---
+++ xMas/merryxmas/Christmas/script.js
@@ -0,0 +1,7 @@
+let sound = document.getElementById("sound");
+sound.innerHTML = "<audio autoplay & loop > <source src='rocking_around_the_christmas_tree_sped_up_1700923757563361487.mp3'> </audio>";
+
+
+
+
+
